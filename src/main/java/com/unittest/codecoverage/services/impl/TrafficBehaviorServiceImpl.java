e1ae31d7884e685e0ed31efa3e2715a59413d8f7
momt99 <mohammad.omt99@yahoo.com>
2021-11-26 21:57:45 +0330

    Upgrade traffic service to use direction

--- src/main/java/com/unittest/codecoverage/services/impl/TrafficBehaviorServiceImpl.java
+++ src/main/java/com/unittest/codecoverage/services/impl/TrafficBehaviorServiceImpl.java
@@ -2,28 +2,34 @@ package com.unittest.codecoverage.services.impl;
 
 import com.unittest.codecoverage.exceptions.BehaviorException;
 import com.unittest.codecoverage.models.Footpassenger;
+import com.unittest.codecoverage.models.StreetDirectionFlow;
 import com.unittest.codecoverage.models.Traffic;
 import com.unittest.codecoverage.models.TrafficLigth;
 import com.unittest.codecoverage.services.TrafficBehaviorService;
 
 public class TrafficBehaviorServiceImpl implements TrafficBehaviorService {
 
-	@Override
-	public void footpassengerCrossTheStreet(Traffic currentTrafic, Footpassenger currentFootpassengerBehavior) {
-		
-		if(TrafficLigth.RED.equals(currentFootpassengerBehavior.getCrossedTrafficLigth()) && 
-				currentFootpassengerBehavior.crossedTheRoad() &&
-				currentTrafic.intenseCarTraffic()) {
-			throw new BehaviorException("You should'nt do that, reckless person");
-		}
-		
-		if(TrafficLigth.GREEN.equals(currentFootpassengerBehavior.getCrossedTrafficLigth()) &&
-				currentTrafic.intenseCarTraffic() && 
-				!currentFootpassengerBehavior.lookedToTheLeft() && 
-				!currentFootpassengerBehavior.lookedToTheRight()) {
-			throw new BehaviorException("You should be more careful");
-		}
-		
-	}
+    @Override
+    public void footpassengerCrossTheStreet(Traffic currentTrafic, Footpassenger currentFootpassengerBehavior) {
 
+        if (TrafficLigth.RED.equals(currentFootpassengerBehavior.getCrossedTrafficLigth()) &&
+                currentFootpassengerBehavior.crossedTheRoad() &&
+                currentTrafic.intenseCarTraffic()) {
+            throw new BehaviorException("You should'nt do that, reckless person");
+        }
+
+        if (TrafficLigth.GREEN.equals(currentFootpassengerBehavior.getCrossedTrafficLigth()) &&
+                currentTrafic.intenseCarTraffic()) {
+            if (currentTrafic.getStreetDirectionFlow() == StreetDirectionFlow.ONE_WAY
+                    && !(currentFootpassengerBehavior.lookedToTheLeft() ||
+                    currentFootpassengerBehavior.lookedToTheRight())
+                    || currentTrafic.getStreetDirectionFlow() == StreetDirectionFlow.TWO_WAY
+                    && !(currentFootpassengerBehavior.lookedToTheLeft() &&
+                    currentFootpassengerBehavior.lookedToTheRight())) {
+                throw new BehaviorException("You should be more careful");
+            }
+
+        }
+
+    }
 }
--- src/test/java/com/unittest/codecoverage/service/TrafficBehaviorServiceTest.java
+++ src/test/java/com/unittest/codecoverage/service/TrafficBehaviorServiceTest.java
@@ -1,5 +1,6 @@
 package com.unittest.codecoverage.service;
 
+import com.unittest.codecoverage.models.StreetDirectionFlow;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -33,15 +34,16 @@ public class TrafficBehaviorServiceTest {
 	
 	@Test
 	@DisplayName("Should throw exception when footpassenger crosses the road during heavy traffic without attention")
-	public void testFootpassengerCrossTheStreet_shouldThrowBehaviorExceptionWhenFootpassengerCrossesTheRoadDuringHeavyTrafficWithoutLookSides() {
+	public void testFootpassengerCrossTheStreet_shouldThrowBehaviorExceptionWhenFootpassengerCrossesTheRoadDuringHeavyTrafficWithoutLookBothSidesWhenTwoWay() {
 	
 		Traffic currentTrafic = new Traffic();
 		currentTrafic.setIntenseCarTraffic(true);
+		currentTrafic.setStreetDirectionFlow(StreetDirectionFlow.TWO_WAY);
 		
 		Footpassenger currentFootpassengerBehavior = new Footpassenger();
 		currentFootpassengerBehavior.setCrossedTheRoad(true);
 		currentFootpassengerBehavior.setCrossedTrafficLigth(TrafficLigth.GREEN);
-		currentFootpassengerBehavior.setLookedToTheLeft(false);
+		currentFootpassengerBehavior.setLookedToTheLeft(true);
 		currentFootpassengerBehavior.setLookedToTheRight(false);
 		
 		Assertions.assertThatThrownBy(() -> trafficBehaviorService.footpassengerCrossTheStreet(currentTrafic, currentFootpassengerBehavior))
@@ -50,4 +52,23 @@ public class TrafficBehaviorServiceTest {
 		
 	}
 
+	@Test
+	@DisplayName("Should throw exception when footpassenger crosses the road during heavy traffic without attention")
+	public void testFootpassengerCrossTheStreet_shouldThrowBehaviorExceptionWhenFootpassengerCrossesTheRoadDuringHeavyTrafficWithoutLookAnySideWhenOneWay() {
+
+		Traffic currentTrafic = new Traffic();
+		currentTrafic.setIntenseCarTraffic(true);
+		currentTrafic.setStreetDirectionFlow(StreetDirectionFlow.ONE_WAY);
+
+		Footpassenger currentFootpassengerBehavior = new Footpassenger();
+		currentFootpassengerBehavior.setCrossedTheRoad(true);
+		currentFootpassengerBehavior.setCrossedTrafficLigth(TrafficLigth.GREEN);
+		currentFootpassengerBehavior.setLookedToTheLeft(false);
+		currentFootpassengerBehavior.setLookedToTheRight(false);
+
+		Assertions.assertThatThrownBy(() -> trafficBehaviorService.footpassengerCrossTheStreet(currentTrafic, currentFootpassengerBehavior))
+				.isInstanceOf(BehaviorException.class)
+				.hasMessage("You should be more careful");
+
+	}
 }
